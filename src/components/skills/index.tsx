487f97e6224f641c507c3187f7917a446913ae6e
Raphael de Moura <raphaelmoura.dev@gmail.com>
2026-04-28 01:16:20 -0300

    Icones na skill front-end

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -10,14 +10,63 @@ export default function Skills() {
             <section className='container-skill'>
                 <aside className='front-details'>
                     <h2>Front-end</h2>
-                    <ul>
-                        <li><strong>Linguagens:</strong> Javascript, Typescript, Node.js, HTML5 e CSS3;</li>
-                        <li><strong>Bibliotecas/Frameworks:</strong> Tailwind css, Next.js, React.js, Expo e Express;</li>
-                        <li><strong>Dados e API:</strong> Consumo de APIs REST, Integração com CMS (Headless);</li>
-                        <li><strong>Libs:</strong> (SSG, SSR, ISR, SPA);</li>
-                        <li><strong>Deploy:</strong> Github Pages, Netlifly e Vercel;</li>
-                        <li><strong>Ferramentas:</strong> npm, Git, GitHub, VITE e VScode.</li>
-                    </ul>
+
+                    <strong>Linguagens:</strong>
+                    <div>
+                        <img src="https://img.shields.io/badge/Javascript-grey?style=for-the-badge&logo=javascript" alt="" />
+                        
+                        <img src="https://img.shields.io/badge/Typescript-grey?style=for-the-badge&logo=typescript" alt="" />
+
+                        <img src="https://img.shields.io/badge/Node.js-grey?style=for-the-badge&logo=node.js" alt="" />
+
+                        <img src="https://img.shields.io/badge/HTML-grey?style=for-the-badge&logo=html5" alt="" />
+
+                        <img src="https://img.shields.io/badge/CSS-grey?style=for-the-badge&logo=css" alt="" />
+                    </div>
+
+                    <strong>Bibliotecas/Frameworks:</strong>
+                    <div>
+                        <img src="https://img.shields.io/badge/React.js-grey?style=for-the-badge&logo=react" alt="" />
+
+                        <img src="https://img.shields.io/badge/Next.js-grey?style=for-the-badge&logo=next.js" alt="" />
+
+                        <img src="https://img.shields.io/badge/Tailwind%20css-grey?style=for-the-badge&logo=tailwindcss" alt="" />
+
+                        <img src="https://img.shields.io/badge/Express-grey?style=for-the-badge&logo=express" alt="" />
+
+                        <img src="https://img.shields.io/badge/Expo-grey?style=for-the-badge&logo=Expo" alt="" />
+                    </div>
+
+                    <strong>Deploy:</strong>
+                    <div>
+                        <img src="https://img.shields.io/badge/Github-grey?style=for-the-badge&logo=github" alt="" />
+
+                        <img src="https://img.shields.io/badge/Vercel-grey?style=for-the-badge&logo=vercel" alt="" />
+
+                        <img src="https://img.shields.io/badge/Netlify-grey?style=for-the-badge&logo=netlify" alt="" />
+                    </div>
+
+                    <strong>Ferramentas:</strong>
+                    <div>
+                        <img src="https://img.shields.io/badge/NPM-grey?style=for-the-badge&logo=npm" alt="" />
+
+                        <img src="https://img.shields.io/badge/Git-grey?style=for-the-badge&logo=git" alt="" />
+
+                        <img src="https://img.shields.io/badge/Vscode-grey?style=for-the-badge&logo=sublime" alt="" />
+
+                        <img src="https://img.shields.io/badge/Vite-grey?style=for-the-badge&logo=vite" alt="" />
+                    </div>
+
+                    <strong>Database:</strong>
+                    <div>
+                        <img src="https://img.shields.io/badge/API%20Rest-grey?style=for-the-badge&logo=" alt="" />
+
+                        <img src="https://img.shields.io/badge/CMS-grey?style=for-the-badge&logo=" alt="" />
+
+                        <img src="https://img.shields.io/badge/Prisma-grey?style=for-the-badge&logo=prisma" alt="" />
+
+                        <img src="https://img.shields.io/badge/Mongo_db-grey?style=for-the-badge&logo=mongodb" alt="" />
+                    </div>
                 </aside>
 
                 <aside className='design-details'>
